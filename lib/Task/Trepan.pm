232de4f5e042cfbba355b25ef846d157fed86d9e
rocky <rocky@cpan.org>
2012-01-16 10:59:41 -0500

    Get ready for 1.0 release. Go over package versions

--- lib/Task/Trepan.pm
+++ lib/Task/Trepan.pm
@@ -1,10 +1,9 @@
-### In lib/Task/CGI/Application.pm
 package Task::Trepan.pm;
 
 use strict;
 use vars qw( $VERSION );
 BEGIN {
-    $VERSION = '0.01';
+    $VERSION = '1.0';
 }
 
 'Is that all there is to it?';
